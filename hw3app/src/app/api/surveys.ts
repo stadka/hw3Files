3f5beae8c052ef343e78c20ab070731668003468
satwik <satwiksai10@gmail.com>
2026-07-11 13:21:22 -0400

    fixed front and back end. added base helm charts that need editing. added docker files. Need to run and test docker file on ec2

--- hw3app/src/app/api/surveys.ts
+++ hw3app/src/app/api/surveys.ts
@@ -1,7 +1,9 @@
+// This file is responsible for setting up the axios instance that will be used to make API calls to the backend.
+// Completed by Matt Courchaine and Satwik Tadikamalla
 import axios from "axios";
 
 export const surveysApi = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? "http://127.0.0.1:8000",
   headers: {
     "Content-Type": "application/json",
   },
