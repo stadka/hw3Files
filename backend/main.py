d0591ae8f70b0e074663c1b71f989212c916c572
satwik <satwiksai10@gmail.com>
2026-07-12 16:22:43 -0400

    fixed cors and mysql requirements

--- backend/main.py
+++ backend/main.py
@@ -47,6 +47,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://ec2-100-57-252-87.compute-1.amazonaws.com:30002"
     ],
     allow_credentials=True,
     allow_methods=["*"],
